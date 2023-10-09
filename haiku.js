be5b9f922bcbd8293af263c972b6f1fe34d9e6b3
wolfgames <36765@ma-web.nl>
2023-10-09 09:48:21 +0200

    goose

--- haiku.js
+++ haiku.js
@@ -3,24 +3,23 @@
 //plak hier de haiku functions onder elkaar
 
 function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+     console.log("haiku by:","Matsuo Bashō");
+     console.log("Over-ripe sushi");
+     console.log("The Master");
+     console.log("Is full of regret");
 }
 
 function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+     console.log("haiku by:","Murakami Kijo");
+     console.log("The lamp once out");
+     console.log("Cool stars enter     ");
+     console.log("The window frame.     ");
 }
 
 function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+     console.log("haiku by:","-Kato Shuson ");
+     console.log("forest, winds howl in rage  ");
+     console.log("with no leaves to blow. ");
 }
 
 
